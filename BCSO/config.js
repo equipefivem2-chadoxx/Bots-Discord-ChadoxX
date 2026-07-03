e7d4e1dad308d28301cfb1253875ef06e97c09d2
Enzo <chadoxx75@gmail.com>
2026-07-03 15:20:33 +0200

    maj push

---
+++ BCSO/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // 🔴 IDs du Serveur de TEST
+    guildId: "1522570075603996703",
+    ticketCategoryId: "1522570076073627719", 
+    archiveChannelId: "1522592674094387210",
+    bcsoRoleId: "ID_ROLE_TEST_STAFF",
+
+    // 🎨 Design & Visuel (Facile à modifier pour la prod)
+    embedColor: "#F5F5DC", // Code couleur Beige clair (tu peux mettre un Hex code vert si tu préfères)
+    logoUrl: "https://cdn.discordapp.com/attachments/1247266103420260585/1522592774388580503/lknfsdqkjnfsdqkjnsfdsdfn.png?ex=6a490885&is=6a47b705&hm=5891952ab9af0ae521e98e5af3f16897a27ebacabd82132734d84411d4c83afa&" // Colle le lien de ton image .png ici !
+};
