9d06f329fcc86ac102079a6f9ea54d1dd7a1ad4b
Enzo <chadoxx75@gmail.com>
2026-07-08 17:29:34 +0200

    maj push

--- BCSO/config.js
+++ BCSO/config.js
@@ -1,13 +1,23 @@
 module.exports = {
     // 🔴 IDs du Serveur
     guildId: "1427651123606589442",
-    ticketCategoryId: "1427847738665472030", 
+    ticketCategoryId: "1427847738665472030",
     archiveChannelId: "1427847879799869440",
-    
+
+    // ☁️ Cloudinary
+    cloudinary: {
+        cloudName: "chadoxx",
+        apiKey: "284719725247594",
+        apiSecret: "rBArt6gG7JSQOkupNyM78GFg9VQ"
+    },
+
     // ⚠️ ATTENTION : Si ces rôles n'existent pas sur ce serveur, la création du ticket va crasher !
-    allowedRolesCommand: ["1427651123606589446", "1427651124286197869"],
+    allowedRolesCommand: [
+        "1427651123606589446",
+        "1427651124286197869"
+    ],
 
     // 🎨 Design & Visuel
-    embedColor: "#F5F5DC",
+    embedColor: "#F5F5BE",
     logoUrl: "https://cdn.discordapp.com/attachments/1247266103420260585/1522592774388580503/lknfsdqkjnfsdqkjnsfdsdfn.png?ex=6a490885&is=6a47b705&hm=5891952ab9af0ae521e98e5af3f16897a27ebacabd82132734d84411d4c83afa&"
 };
