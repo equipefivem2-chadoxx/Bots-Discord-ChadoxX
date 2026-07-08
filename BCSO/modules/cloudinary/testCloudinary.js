bcecfc7be9c092969c99a01d1f6b792d014c1df5
Enzo <chadoxx75@gmail.com>
2026-07-08 17:43:01 +0200

    maj push

--- BCSO/modules/cloudinary/testCloudinary.js
+++ BCSO/modules/cloudinary/testCloudinary.js
@@ -1,11 +1,5 @@
-const cloudinary = require("./cloudinary");
-
 module.exports = () => {
-    cloudinary.api.ping()
-        .then(() => {
-            console.log("✅ Cloudinary connecté !");
-        })
-        .catch((error) => {
-            console.error("❌ Erreur Cloudinary :", error);
-        });
+    console.log("CLOUD NAME =", process.env.CLOUDINARY_CLOUD_NAME);
+    console.log("API KEY =", process.env.CLOUDINARY_API_KEY ? "OK" : "MANQUANT");
+    console.log("SECRET =", process.env.CLOUDINARY_API_SECRET ? "OK" : "MANQUANT");
 };
